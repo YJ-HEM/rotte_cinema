95a45774e7c13f3603465c435045f75aad9f4481
eunji <68682423a@gmail.com>
2021-06-04 17:04:10 +0900

    브랜치테스트

---
+++ rotte_cinema/Manager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace rotte_cinema
+{
+    class Manager
+    {
+        private static MySqlConnection connection = new MySqlConnection("Server = db.kumas.dev; port=3306; database=Cinema; uid=user; pwd=0000")
+
+
+        public void Input(string sql) { 
+            
+        }             
+            
+    }
+
+}
