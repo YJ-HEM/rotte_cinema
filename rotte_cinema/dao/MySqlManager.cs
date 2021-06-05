d16b40f55727fa88ebee6e6fde77024f7ad392c0
HwanSu Kim <kuma2301@naver.com>
2021-06-05 19:31:05 +0900

    쿼리문 자동으로 대문자로 변경되도록 설정

--- rotte_cinema/dao/MySqlManager.cs
+++ rotte_cinema/dao/MySqlManager.cs
@@ -36,7 +36,7 @@ namespace rotte_cinema.dao
 
 		public static int Input(string sql)
 		{
-			using (MySqlCommand command = new MySqlCommand(sql, connection))
+			using (MySqlCommand command = new MySqlCommand(sql.ToUpper(), connection))
 			{
 				connection.Open();
 				int result = command.ExecuteNonQuery(); /* ExecuteNonQuery 리턴값은 영향을 받은 행의 갯수 롤백이 발생한다면 -1을 리턴 */
@@ -47,7 +47,7 @@ namespace rotte_cinema.dao
 
 		public static int Delete(string sql)
 		{
-			using (MySqlCommand command = new MySqlCommand(sql, connection))
+			using (MySqlCommand command = new MySqlCommand(sql.ToUpper(), connection))
 			{
 				connection.Open();
 				int result = command.ExecuteNonQuery(); /* ExecuteNonQuery 리턴값은 영향을 받은 행의 갯수 롤백이 발생한다면 -1을 리턴 */
@@ -59,7 +59,7 @@ namespace rotte_cinema.dao
 
 		public static int Insert(string sql)
 		{
-			using (MySqlCommand command = new MySqlCommand(sql, connection))
+			using (MySqlCommand command = new MySqlCommand(sql.ToUpper(), connection))
 			{
 				connection.Open();
 				int result = command.ExecuteNonQuery(); /* ExecuteNonQuery 리턴값은 영향을 받은 행의 갯수 롤백이 발생한다면 -1을 리턴 */
@@ -71,7 +71,7 @@ namespace rotte_cinema.dao
 
 		public static int Update(string sql)
 		{
-			using (MySqlCommand command = new MySqlCommand(sql, connection))
+			using (MySqlCommand command = new MySqlCommand(sql.ToUpper(), connection))
 			{
 				connection.Open();
 				int result = command.ExecuteNonQuery(); /* ExecuteNonQuery 리턴값은 영향을 받은 행의 갯수 롤백이 발생한다면 -1을 리턴 */
@@ -82,7 +82,7 @@ namespace rotte_cinema.dao
 
 		public static object Select(string sql)
 		{
-			using (MySqlCommand command = new MySqlCommand(sql, connection))
+			using (MySqlCommand command = new MySqlCommand(sql.ToUpper(), connection))
 			{
 				connection.Open();
 				object result = command.ExecuteScalar(); /* ExecuteScalar 결과값 하나만을 object 형식으로 반환 */
@@ -94,7 +94,7 @@ namespace rotte_cinema.dao
 		public static DataTable AdapterRead(string sql)
 		{
 			DataTable dt = new DataTable();
-			MySqlDataAdapter adapter = new MySqlDataAdapter(sql, connection); /* 데이터 테이블 형식으로 반환. */
+			MySqlDataAdapter adapter = new MySqlDataAdapter(sql.ToUpper(), connection); /* 데이터 테이블 형식으로 반환. */
 			adapter.Fill(dt);
 			return dt;
 		}
