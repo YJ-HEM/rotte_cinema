376c350afeb583b7fbf610dbbf8806a20216894c
eunji <68682423a@gmail.com>
2021-06-18 02:19:29 +0900

    상단바 삭제, 하단바 구현중, 뒤로가기 구현

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -19,6 +19,7 @@ import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
@@ -32,10 +33,11 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class MainActivity extends AppCompatActivity {
     private WebView mWebView;
     BottomNavigationView mBottomNV;
+    private FragmentManager fm;
+    private FragmentTransaction ft;
 
     Fragment1 fragment1;
     Fragment2 fragment2;
-    Fragment3 fragment3;
 //수정
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,20 +61,56 @@ public class MainActivity extends AppCompatActivity {
 
 
         mBottomNV = findViewById(R.id.bottom);
-        mBottomNV.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-          @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()) { //menu_bottom.xml에서 지정해줬던 아이디 값을 받아와서 각 아이디값마다 다른 이벤트를 발생시킵니다.
-                    case R.id.tab1:{ getSupportFragmentManager().beginTransaction() .replace(R.id.webView,fragment1).commitAllowingStateLoss();
-                    break;}
-                    case R.id.tab2:{ getSupportFragmentManager().beginTransaction() .replace(R.id.webView,fragment2).commitAllowingStateLoss();break; }
-                    case R.id.tab3:{ getSupportFragmentManager().beginTransaction() .replace(R.id.webView,fragment3).commitAllowingStateLoss(); break   ; }
-                     }
-              return true;
-          } });
-
-
+        mBottomNV.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
+            {
+                switch (menuItem.getItemId())
+                {
+                    case R.id.tab1:
+                        setFrag(0);
+                        break;
+                    case R.id.tab2:
+                        setFrag(1);
+                        break;
+                    case R.id.tab3:
+                        setFrag(2);
+                        break;
                 }
+                return true;
+            }
+        });
+
+        fragment1=new Fragment1();
+        fragment2=new Fragment2();
+        //setFrag(0); // 첫 프래그먼트 화면 지정
+    }
+
+    // 프레그먼트 교체
+    private void setFrag(int n)
+    {
+        fm = getSupportFragmentManager();
+        ft= fm.beginTransaction();
+        switch (n)
+        {
+            case 0:
+               mWebView.loadUrl("https://lesslate.github.io/android/안드로이드-하단-네비게이션(Bottom-Navigation)-추가하기/");
+                break;
+
+            case 1:
+                ft.replace(R.id.webView,fragment1);
+                ft.commit();
+                break;
+
+            case 2:
+                ft.replace(R.id.webView,fragment2);
+                ft.commit();
+                break;
+
+
+        }
+    }
 
 
                 public class SslWebViewConnect extends WebViewClient {
