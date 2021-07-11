ea387477a46cdab216a90fff525df8a48775b24c
Administrator <68682423a@gmail.com>
2021-07-11 18:21:16 +0900

    temp_commit

--- app/src/main/java/com/example/lottecinema/watchedMovies.java
+++ app/src/main/java/com/example/lottecinema/watchedMovies.java
@@ -10,8 +10,17 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.lottecinema.Adapter.WatchedMoviesAdapter;
+import com.example.lottecinema.databinding.ActivityWatchedMoviesBinding;
+import com.example.lottecinema.databinding.WatchedmoviesItemBinding;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class watchedMovies extends Fragment {
 
@@ -33,21 +42,31 @@ public class watchedMovies extends Fragment {
     @org.jetbrains.annotations.Nullable
     @Override
     public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        // 리사이클러뷰에 표시할 데이터 리스트 생성.
+        ArrayList<String> list = new ArrayList<>();
+        for (int i=0; i<10; i++) {
+            list.add(String.format("TEXT %d", i)) ;
+        }
+
+        ActivityWatchedMoviesBinding binding = ActivityWatchedMoviesBinding.inflate(inflater);
 
 
+        // 리사이클러뷰에 LinearLayoutManager 객체 지정.
+
+        RecyclerView recyclerView = binding.recyclerViewWatchedList;
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        // 리사이클러뷰에 SimpleTextAdapter 객체 지정.
+        WatchedMoviesAdapter adapter = new WatchedMoviesAdapter(list) ;
+        recyclerView.setAdapter(adapter);
 
         return inflater.inflate(R.layout.activity_watched_movies, container, false);
-
-
-
 
     }
     void navigationMenu(Button button, String url) {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
             }
         });
     }
